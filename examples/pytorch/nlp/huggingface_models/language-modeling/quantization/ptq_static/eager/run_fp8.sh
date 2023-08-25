106205be2b8b26194b8eef470d84f58181adfea8
Xin He <xin3.he@intel.com>
2023-08-25 14:09:18 +0800

    add example for llm
    Signed-off-by: Xin He <xin3.he@intel.com>

---
+++ examples/pytorch/nlp/huggingface_models/language-modeling/quantization/ptq_static/eager/run_fp8.sh
@@ -0,0 +1,20 @@
+set -x
+
+model_path='facebook/opt-125m'
+precisions=(
+    'fp8_e5m2',
+    'fp8_e4m3',
+)
+for prec in ${precisions[*]}
+do
+python run_clm_fp8.py --model ${model_path} \
+                        --accuracy  --ipex --calib_iters 1 \
+                        --tasks lambada_openai --sq \
+                        --batch_size 32 --int8  \
+                        --alpha 0.5 --quantize \
+                        --precision fp8_e5m2 \
+                        --cast_e4m3
+done
+
+
+
