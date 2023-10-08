148d74ecbcd4e281bf6d719c784296c662167824
yiliu30 <yi4.liu@intel.com>
2023-10-08 09:18:11 +0800

    add dependency sampler and ut
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- docs/source/inc_3_development_notes.md
+++ docs/source/inc_3_development_notes.md
@@ -1,5 +1,13 @@
 # Interface between Strategy and adaptor
 
+### TODO list
+
+- Strategy
+    - [x] Base sampler
+    - [ ] Dependency sampler 
+- [] Objective
+- [] Metric
+
 ### Change
 - Removed `query_capability`
     - The adaptor registers its own `sampler`
--- neural_compressor/common/strategy/sampler.py
+++ neural_compressor/common/strategy/sampler.py
@@ -47,6 +47,8 @@ class BaseSampler:
         max_trials=MAX_TRIALS_EACH_SAMPLER,
         priority: float = -1,
         sampling_algo=None,
+        dependence_samplers: List = [],
+        analysis_manager=None,
     ) -> None:
         """Initial an Sampler.
 
@@ -54,16 +56,101 @@ class BaseSampler:
             hp: the hyper parameter space to sample from.
             name: sampler name. Defaults to "".
             max_trials: the maximum number of trials. Defaults to MAX_TRIALS_EACH_SAMPLER.
-            priority: the priority of the sampler, the sampler with higher priority will be sampled first. Defaults to -1.
+            priority: the priority of the sampler, the sampler with higher priority will be sampled first. 
+                Defaults to -1.
             sampling_algo: the sampling algorithm which determines the order of the sampling. Defaults to None.
+            dependence_samplers: the samplers that this sampler depends on. Defaults to [].
+            analysis_manager: the analysis manager for fetching the analysis results of dependence samplers and 
+                register the analysis results of this sampler. Defaults to None.
         """
         self.hp = hp
         self.name = name
         self.max_trials = max_trials
         self.priority = priority
         self.sampling_algo = sampling_algo
+        self._dependence_samplers: List[BaseSampler] = dependence_samplers
+        self._analysis_manager = analysis_manager
+        assert self._post_init_check(), "The sampler is not valid."
+    
+    def _post_init_check(self):
+        return True
+        
+    @property
+    def dependence_samplers(self):
+        """Get the dependence samplers of this sampler."""
+        return self._dependence_samplers
+
+    @dependence_samplers.setter
+    def dependence_samplers(self, dependence_samplers):
+        """Set the dependence samplers of this sampler."""
+        self._dependence_samplers = dependence_samplers
+    
+    def add_dependence_sampler(self, sampler):
+        """Add a dependence sampler.
+        
+        For Example, the `accumulated fallback sampler` depends on the `one by one fallback sampler`.
+        """
+        self._dependence_samplers.append(sampler)
 
     def __iter__(self):
         """Generate a list of configs."""
         for config in self.hp.exapnd():
             yield config
+
+
+class FallbackSampler(BaseSampler):
+    """Fallback sampler.
+
+    Fallback op one by one.
+    """
+
+    def __init__(
+        self,
+        hp: HyperParams,
+        name: str = "",
+        max_trials=MAX_TRIALS_EACH_SAMPLER,
+        priority: float = -1,
+        sampling_algo=None,
+        dependence_samplers: List = [],
+        analysis_manager=None,
+    ) -> None:
+        super().__init__(
+            hp,
+            name,
+            max_trials,
+            priority,
+            sampling_algo,
+            dependence_samplers,
+            analysis_manager,
+        )
+
+
+class AccumulatedFallbackSampler(BaseSampler):
+    """Accumulated fallback sampler.
+
+    Fallback ops accumulated.
+    """
+
+    def __init__(
+        self,
+        hp: HyperParams,
+        name: str = "",
+        max_trials=MAX_TRIALS_EACH_SAMPLER,
+        priority: float = -1,
+        sampling_algo=None,
+        dependence_samplers: List = [],
+        analysis_manager=None,
+    ) -> None:
+        super().__init__(
+            hp,
+            name,
+            max_trials,
+            priority,
+            sampling_algo,
+            dependence_samplers,
+            analysis_manager,
+        )
+    
+    def _post_init_check(self):
+        has_add_fallback_sampler = any([isinstance(sampler, FallbackSampler) for sampler in self.dependence_samplers])
+        return super()._post_init_check() and  has_add_fallback_sampler
