4bec56bf4fbbc437741da37d2ce707d16c9014a0
yiliu30 <yi4.liu@intel.com>
2023-10-20 11:58:42 +0800

    revert change
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- neural_compressor/common/config.py
+++ neural_compressor/common/config.py
@@ -13,6 +13,25 @@
 # limitations under the License.
 
 
+class BaseConfig():
+  def __init__(self):
+      self.scope = {}
+
+  def __add__(self, obj):
+      self.scope = {**self.scope, **obj.scope}
+
+class FP32Config(BaseConfig):
+  def __init__(self, scope):
+      super().__init__()
+      self.scope = {'fp32': scope}
+
+class FP8QuantConfig(BaseConfig):
+  def __init__(self, scheme, scope):
+      super().__init__()
+      assert(scheme in ['fp8_e4m3', 'fp8_e3m4', 'fp8_e5m2']), "The FP8 configuration is wrong! Please double check."
+      self.scope = {scheme: scope}
+
+
 from neural_compressor.common.constant import MAX_TRIALS_EACH_SAMPLER
 
 
