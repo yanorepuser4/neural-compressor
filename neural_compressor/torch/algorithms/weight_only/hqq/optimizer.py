2d77b91a5c45956b2e7e4ade7fb7d2eee3e4965b
yiliu30 <yi4.liu@intel.com>
2024-03-03 10:22:24 +0800

    use hpu
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- neural_compressor/torch/algorithms/weight_only/hqq/optimizer.py
+++ neural_compressor/torch/algorithms/weight_only/hqq/optimizer.py
@@ -44,7 +44,7 @@ def optimize_weights_proximal_legacy(
     device = auto_detect_accelerator().current_device()
 
     # TODO: refine it for cpu device
-    if auto_detect_accelerator().name() == "cuda":
+    if auto_detect_accelerator().name() != "cpu":
         dtype = torch.float16
     else:
         dtype = torch.float32
