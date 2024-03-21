a1f3b352f8a2d70b1bda3e6a3af26b82305798e8
yiliu30 <yi4.liu@intel.com>
2024-03-21 13:56:57 +0800

    load model with bf16
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- examples/autoround_examples/main.py
+++ examples/autoround_examples/main.py
@@ -1,26 +1,35 @@
 
+from neural_compressor.adaptor.torch_utils.auto_round import get_dataloader
 from transformers import AutoModel, AutoTokenizer
- 
+
 from neural_compressor.config import PostTrainingQuantConfig
 from neural_compressor.quantization import fit
+import torch
+
+
+float_model = AutoModel.from_pretrained(
+    "EleutherAI/gpt-neo-125m", torch_dtype=torch.bfloat16)
+
+
 # from auto_round.calib_dataset import CALIB_DATASETS
-from neural_compressor.adaptor.torch_utils.auto_round import get_dataloader
- 
+
 tokenizer = AutoTokenizer.from_pretrained(
-            "EleutherAI/gpt-neo-125m", trust_remote_code=True
-        )
+    "EleutherAI/gpt-neo-125m", trust_remote_code=True
+)
 # get_dataloader = CALIB_DATASETS.get("NeelNanda/pile-10k", CALIB_DATASETS["NeelNanda/pile-10k"])
 dataloader = get_dataloader(tokenizer, seqlen=2048)
-float_model = AutoModel.from_pretrained("EleutherAI/gpt-neo-125m")
+
+
 woq_conf = PostTrainingQuantConfig(approach="weight_only",
-                                    op_type_dict={
-                                        ".*": {  # re.match
+                                   op_type_dict={
+                                       ".*": {  # re.match
                                             "weight": {
-                                            "dtype":"int",
-                                            "bits": 4,  # 1-8 bit
-                                            "algorithm": "AUTOROUND",
+                                                "dtype": "int",
+                                                "bits": 4,  # 1-8 bit
+                                                "algorithm": "AUTOROUND",
                                             },
-                                        }
-                                    }
-                                    )
-quantized_model = fit(model=float_model, conf=woq_conf, calib_dataloader=dataloader)
+                                       }
+                                   }
+                                   )
+quantized_model = fit(model=float_model, conf=woq_conf,
+                      calib_dataloader=dataloader)
