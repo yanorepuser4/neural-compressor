c833aa9fe0cf3a0d50907b6b3f08d756c2799bea
Mengni Wang <mengni.wang@intel.com>
2024-05-30 06:29:59 +0300

    eval test
    Signed-off-by: Mengni Wang <mengni.wang@intel.com>

--- examples/3.x_api/pytorch/nlp/huggingface_models/language-modeling/quantization/mx/run_clm_no_trainer.py
+++ examples/3.x_api/pytorch/nlp/huggingface_models/language-modeling/quantization/mx/run_clm_no_trainer.py
@@ -6,7 +6,7 @@ import torch
 from neural_compressor.torch.utils import accelerator
 
 device = accelerator.current_device_name()
-if "hpu" in device.name:
+if "hpu" in device:
     import habana_frameworks.torch.hpex
     import habana_frameworks.torch.core as htcore
     from habana_frameworks.torch.hpu import memory_stats
@@ -64,6 +64,140 @@ parser.add_argument("--to_graph", action="store_true")
 args = parser.parse_args()
 
 
+def itrex_bootstrap_stderr(f, xs, iters):
+    from lm_eval.metrics import _bootstrap_internal, sample_stddev
+    res = []
+    chunk_size = min(1000, iters)
+    it = _bootstrap_internal(f, chunk_size)
+    for i in range(iters // chunk_size):
+        bootstrap = it((i, xs))
+        res.extend(bootstrap)
+    return sample_stddev(res)
+
+
+def eval_func(user_model, tokenizer, args):
+    import os
+    import re
+    import time
+    import json
+    import torch
+    import habana_frameworks.torch.hpex
+    import torch.nn.functional as F
+    import lm_eval
+    import lm_eval.tasks
+    import lm_eval.evaluator
+
+    # to avoid out-of-memory caused by Popen for large language models.
+    lm_eval.metrics.bootstrap_stderr = itrex_bootstrap_stderr
+
+    class HabanaModelAdapter(lm_eval.base.BaseLM):
+        def __init__(self, tokenizer, model, args, options):
+            super().__init__()
+            self.tokenizer = tokenizer
+            self.model = model.eval()
+            self._batch_size = args.batch_size
+            self.buckets = list(sorted(args.buckets))
+            self.options = options
+            self._device = "hpu"
+            torch.set_grad_enabled(False)
+
+        @property
+        def eot_token_id(self):
+            return self.model.config.eos_token_id
+
+        @property
+        def max_length(self):
+            return self.buckets[-1]
+
+        @property
+        def max_gen_toks(self):
+            raise NotImplementedError()
+
+        @property
+        def batch_size(self):
+            return self._batch_size
+
+        @property
+        def device(self):
+            # We need to do padding ourselves, otherwise we'll end up with recompilations
+            # Returning 'cpu' to keep tensors on CPU in lm_eval code
+            return 'cpu' # 'hpu'
+
+        def tok_encode(self, string):
+            if (
+                re.search("chatglm3", args.model.lower()) or
+                re.search("llama", args.model.lower()) or
+                re.search("mistral", args.model.lower())
+            ):
+                string = string.lstrip()
+            return self.tokenizer.encode(string, add_special_tokens=False)
+
+        def tok_decode(self, tokens):
+            return self.tokenizer.decode(tokens, skip_special_tokens=True)
+
+        def _model_generate(self, context, max_length, eos_token_id):
+            raise NotImplementedError()
+
+        def find_bucket(self, length):
+            return [b for b in self.buckets if b >= length][0]
+
+        def _model_call(self, inputs):
+            seq_length = inputs.shape[-1]
+            padding_length = 0
+            bucket_length = self.find_bucket(seq_length)
+            padding_length = bucket_length - seq_length
+            inputs = F.pad(inputs, (0, padding_length), value=self.model.config.pad_token_id)
+            logits = self.model(inputs.to(self._device))["logits"].cpu()
+
+            if padding_length > 0:
+                logits = logits[:, :-padding_length, :]
+            logits = logits.to(torch.float32)
+            return logits
+
+    lm_tasks = lm_eval.tasks.get_task_dict(args.tasks)
+    options = None
+    lm = HabanaModelAdapter(tokenizer, user_model, args, options)
+
+    eval_start = time.perf_counter()
+    if args.approach == "cast":
+        from neural_compressor.torch.amp import autocast
+        if args.precision == "fp8_e4m3":
+            dtype = torch.float8_e4m3fn
+        elif args.precision == "fp8_e5m2":
+            dtype = torch.float8_e5m2
+        elif args.precision == "fp16":
+            dtype = torch.float16
+        elif args.precision == "bf16":
+            dtype = torch.bfloat16
+        with autocast('hpu', dtype=dtype):
+            results = lm_eval.evaluator.evaluate(lm, lm_tasks, limit=args.limit)
+    else:
+        results = lm_eval.evaluator.evaluate(lm, lm_tasks, limit=args.limit)
+    print(lm_eval.evaluator.make_table(results))
+    eval_end = time.perf_counter()
+    print("Duration:", eval_end - eval_start)
+    results['args'] = vars(args)
+    results['duration'] = eval_end - eval_start
+
+    # make sure that result is dumped only once during multi-cards evaluation
+    local_rank = int(os.getenv('LOCAL_RANK', '-1'))
+    if local_rank in [-1, 0]:
+        dumped = json.dumps(results, indent=2)
+        accu_dict = {}
+        case_name = str(args.approach) + "-" + args.precision
+        for task_name in args.tasks:
+            if task_name == "wikitext":
+                print("Accuracy for %s is: %s" % (task_name, results["results"][task_name]["word_perplexity"]), flush=True)
+                accu_dict[task_name] = [args.model, case_name, results["results"][task_name]["word_perplexity"]]
+            else:
+                print("Accuracy for %s is: %s" % (task_name, results["results"][task_name]["acc"]), flush=True)
+                accu_dict[task_name] = [args.model, case_name, results["results"][task_name]["acc"]]
+        accu_dict["duration"] = [args.model, case_name, results["duration"]]
+        if args.dump_to_excel:
+            save_to_excel(accu_dict)
+    return results["results"][task_name]["acc"]
+
+
 def get_user_model():
     from transformers import AutoModelForCausalLM, AutoModel, AutoTokenizer
     user_model = AutoModelForCausalLM.from_pretrained(
@@ -87,7 +221,7 @@ if args.quantize:
     quant_config = MXQuantConfig(w_dtype=args.w_dtype, act_dtype=args.act_dtype, weight_only=args.woq)
     user_model = quantize(model=user_model, quant_config=quant_config)
 
-if "hpu" in device.name:
+if "hpu" in device:
     if args.to_graph:
         # inference optimization
         import habana_frameworks.torch.hpu.graphs as htgraphs
@@ -95,6 +229,8 @@ if "hpu" in device.name:
     show_msg()
 
 if args.accuracy:
+    eval_func(user_model, tokenizer=tokenizer, args=args)
+    import pdb;pdb.set_trace()
     from intel_extension_for_transformers.transformers.llm.evaluation.lm_eval import evaluate, LMEvalParser
     eval_args = LMEvalParser(
         model="hf",
@@ -103,6 +239,7 @@ if args.accuracy:
         batch_size=args.batch_size,
         tasks=','.join(args.tasks),
         device=device,
+        limit=10,
     )
     results = evaluate(eval_args)
     dumped = json.dumps(results, indent=2)
